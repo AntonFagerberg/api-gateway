a1f87b2c31f370aacf6db99aa676e5cd3104b00e
Anton Fagerberg <anton.fagerberg@jayway.com>
2015-01-14 16:09:38 +0100

    User JSON example

---
+++ app/json/User.scala
@@ -0,0 +1,44 @@
+package json
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+
+object User extends Controller {
+  val jsonString =
+    """
+      |{
+      |    "data": {
+      |        "id": "1574083",
+      |        "username": "snoopdogg",
+      |        "full_name": "Snoop Dogg",
+      |        "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg",
+      |        "bio": "This is my bio",
+      |        "website": "http://snoopdogg.com",
+      |        "counts": {
+      |            "media": 1320,
+      |            "follows": 420,
+      |            "followed_by": 3410
+      |        }
+      |    }
+      |}
+    """.stripMargin
+
+  def getUser(s: String) = Action {
+    Ok(
+      Json.parse(
+        jsonString
+      )
+    )
+  }
+
+  def getUsers(s: String) = Action {
+    val jsonArrayString = List.fill(10)(jsonString).mkString("[", ",", "]")
+
+    Ok(
+      Json.parse(
+        jsonArrayString
+      )
+    )
+  }
+
+}
